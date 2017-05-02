3e20e8d0574f6aa71efaf5acdc1071ba7daea1f7
Tom <p.tommy93@gmail.com>
2017-05-02 17:30:49 +0200

    Merge https://github.com/angular/material/pull/10549

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+angular.module('docsApp').constant('BUILDCONFIG', {
+  "ngVersion": "1.5.5",
+  "version": "1.1.4",
+  "repository": "https://github.com/angular/material",
+  "commit": "cd2c9b60b80adc2fd512155ff37c09e492f945ac",
+  "date": "2017-05-02 17:24:45 +0200"
+});
